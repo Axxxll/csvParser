49ea0ce383e10cfa4d7e1771b965f10c3f2361ce
Axxxll <alex.plam.atan@gmail.com>
2022-08-05 17:26:40 +0300

    Extract account id into seperate variable

--- index.js
+++ index.js
@@ -1,41 +1,24 @@
-// const object = {
-//     "name": "test"
-// }
-// let tokenName = "MANA"
-
-// object.name = "new name"
-
-// object.age = 12
-// object.autor = "Axl"
-// object[tokenName] = 12
-// object[tokenName] += 12
-
-// tokenName = "DAO"
-
-// object[tokenName] = 12
-
-// console.log(object);
-
 const fs = require('fs')
 const csv = require('csv-parser')
 const resultsOfCsv = []  // Stores innitialy parsed csv document
 const result = [] // Stores result of function 
+const accId = '0xaf5a05ebe2d24c5c6a6be222f824a8a0c170daec'
 let  obj = {}
 
 fs.createReadStream('export-address-token-0xAf5a05ebe2D24C5C6A6bE222F824A8a0C170dAEC.csv')
   .pipe(csv())
   .on('data', (data) => resultsOfCsv.push(data))
-  .on('end', doSomething)
+  .on('end', parseCsvDoc)
 
-function doSomething() {
+function parseCsvDoc() {
     for (let i = 0; i < resultsOfCsv.length; i++) {
       let leBalance = resultsOfCsv[i].TokenName
       if(!obj[leBalance]) {
         obj[leBalance] = 0
       }
-      if (resultsOfCsv[i].To === '0xaf5a05ebe2d24c5c6a6be222f824a8a0c170daec') {
+      if (resultsOfCsv[i].To === accId) {
         obj[leBalance] += Number(resultsOfCsv[i].Value.replace(',', ""))
-      } else if (resultsOfCsv[i].From === '0xaf5a05ebe2d24c5c6a6be222f824a8a0c170daec'){
+      } else if (resultsOfCsv[i].From === accId){
         obj[leBalance] -= Number(resultsOfCsv[i].Value.replace(',', ""))
       }
       let time = resultsOfCsv[i].UnixTimestamp
@@ -48,4 +31,5 @@ function doSomething() {
       result.push(res)
     }
     fs.writeFileSync('tokenStorage.json', JSON.stringify(result))
+    console.log(obj)
 }
